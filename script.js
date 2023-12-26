69b487d10f110778ab3dea79445d9f4f8446f2b5
Matheus Oliveira <matheus.oliveira6842@gmail.com>
2023-12-26 20:28:56 -0300

    Reorganizando

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function carregar(){
-    var fsev = document.getElementsByName('criterio')
+    var fregiao = document.getElementsByName('regiao')
+    var faltura = document.getElementsByName('altura')
+    var fprop = document.getElementsByName('propagacao')
+    var ftempo = document.getElementsByName('operacao')
+    var ftam = document.getElementsByName('tamanho')
+    var fluz = document.getElementsByName('luz')
+
     var laudo = document.getElementById('laudo')
- if(fsev[0].checked){
-    laudo.innerHTML = `Severidade 0`
-    
- } else if(fsev[1].checked){
+ if(fregiao[1].checked && faltura[0].checked ){
+    laudo.innerHTML = `Severidade 0` 
+ } else if(faltura[0].checked && fprop[1].checked){
+    laudo.innerHTML = `Severidade 0`   
+ }else if(ftempo[0].checked && fprop[1].checked){
     laudo.innerHTML = `Severidade 1`
- 
-} else if(fsev[2].checked){
+ } else if(ftempo[0].checked && ftam[0].checked && fprop[0].checked ){
     laudo.innerHTML = `Severidade 2`
- 
-} else if(fsev[3].checked){
+ }else if(ftempo[1].checked && ftam[0].checked && fprop[0].checked){
     laudo.innerHTML = `Severidade 3`
- 
-} else if(fsev[4].checked){
+ } else if(fprop[0].checked && ftam[0].checked && fluz[1].checked){
     laudo.innerHTML = `Severidade 4`
- 
-} else if(fsev[5].checked){
+ } else if(fprop[0].checked && ftam[1].checked || fluz[0].checked){
     laudo.innerHTML = `Severidade 5`
- 
-} 
+ }
 
+ }
 
-}
